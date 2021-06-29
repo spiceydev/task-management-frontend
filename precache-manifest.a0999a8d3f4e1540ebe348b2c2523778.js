3c84f47e28c21ecd1ae920a3c4a4d396fd864118
spiceydev <spiceydev@users.noreply.github.com>
2021-06-29 01:14:52 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.a0999a8d3f4e1540ebe348b2c2523778.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41fa0579a9baab8aaef90d7f2e89644e",
+    "url": "https://spiceydev.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "07f8bda0491bb04a78eb",
+    "url": "https://spiceydev.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c66007b2c5d3987b8954",
+    "url": "https://spiceydev.github.io/task-management-frontend/static/js/2.f02ce6a4.chunk.js"
+  },
+  {
+    "revision": "07f8bda0491bb04a78eb",
+    "url": "https://spiceydev.github.io/task-management-frontend/static/js/main.088b24a2.chunk.js"
+  },
+  {
+    "revision": "e481fe8030cf78b7018e",
+    "url": "https://spiceydev.github.io/task-management-frontend/static/js/runtime~main.f27e0ae2.js"
+  }
+]);
